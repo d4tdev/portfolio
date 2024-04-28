4f097c2292f2a781efb3581df75bdbd0a8ea1227
d4Jt <jsmsj04@gmail.com>
2024-04-28 16:14:41 +0700

    Update projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -6,6 +6,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import TransitionEffect from '@/components/TransitionEffect';
 
 import projectDiscord from '../../public/images/projects/discordv14_thumbnail.png';
 import projectAPIXSMB from '../../public/images/projects/xsmb_thumbnail.png';
@@ -118,6 +119,7 @@ const projects = () => {
             <title> ttd4t | Projects Page</title>
             <meta name="description" content="Some projects made by me" />
          </Head>
+         <TransitionEffect />
          <main className="w-full mb-16 flex flex-col items-center justify-center">
             <Layout className="pt-16 flex flex-col justify-center items-center">
                <AnimatedText
